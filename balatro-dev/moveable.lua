a8b5c52ba81a5a6c0ca73372d93124766460089a
LnxFCA <dev@lnxfca.com>
2025-03-21 20:41:12 -0500

    balatro-dev: Finish Moveable

--- balatro-dev/moveable.lua
+++ balatro-dev/moveable.lua
@@ -1,6 +1,14 @@
 ---@meta
 
 
+---@class BALATRO.Moveable.ARGS.get_major
+---@field major BALATRO.Moveable
+---@field offset BALATRO.Node.Point
+
+
+---@class BALATRO.Moveable.ARGS : BALATRO.Node.ARGS
+---@field get_major BALATRO.Moveable.ARGS.get_major
+
 ---@class BALATRO.Moveable.Frame : BALATRO.Node.Frame
 ---@field OLD_MAJOR BALATRO.Moveable
 ---@field MAJOR BALATRO.Moveable
@@ -12,9 +20,29 @@
 ---@field mag number
 
 
----@alias BALATRO.Moveable.Role.Type string | "Major" | "Minor"
+---@alias BALATRO.Moveable.Role.Type string | "Major" | "Minor" | "Glued"
 ---@alias BALATRO.Moveable.Role.Bond string | "Strong" | "Weak"
----@alias BALATRO.Moveable.Alignment.Type string | "a"
+---@alias BALATRO.Moveable.Alignment.Type string | 'a' | 'm' | 'c' | 'b' | 't' | 'l' | 'r' | 'i'
+
+
+---@class BALATRO.Moveable.Alignment.TypeList
+---@field a boolean
+---@field m? integer
+---@field c? integer
+---@field b? integer
+---@field t? integer
+---@field l? integer
+---@field r? integer
+---@field i? integer
+
+
+---@class BALATRO.Moveable.Role.Common
+---@field major? BALATRO.Moveable
+---@field xy_bond? BALATRO.Moveable.Role.Bond
+---@field wh_bond? BALATRO.Moveable.Role.Bond
+---@field r_bond? BALATRO.Moveable.Role.Bond
+---@field scale_bond? BALATRO.Moveable.Role.Bond
+---@field offset? BALATRO.Node.Point
 
 
 ---@class BALATRO.Moveable.Role
@@ -44,6 +72,7 @@
 ---@field prev_type BALATRO.Moveable.Alignment.Type
 ---@field prev_offset BALATRO.Node.Point
 ---@field lr_clamp? number
+---@field type_list? BALATRO.Moveable.Alignment.TypeList
 
 
 ---@alias BALATRO.Moveable.Pinch { x: boolean, y: boolean }
@@ -55,9 +84,12 @@
 ---@field extend fun(self:BALATRO.Moveable): BALATRO.Moveable.Super
 
 
+---@class BALATRO.Moveable.Config : BALATRO.Node.Config
+---@field refresh_movement? boolean
+
+
 ---@class BALATRO.Moveable : BALATRO.Moveable.Class
----@field T BALATRO.Node.Transform
----@field VT BALATRO.Node.Transform
+---@field ARGS BALATRO.Moveable.ARGS
 ---@field FRAME BALATRO.Moveable.Frame
 ---@field NEW_ALIGNMENT boolean
 ---@field juice BALATRO.Moveable.Juice
@@ -73,56 +105,75 @@
 ---@field shadow_parrallax BALATRO.Node.Point
 ---@field layered_parallax BALATRO.Node.Point
 ---@field shadow_height number
-Moveable = Node:extend()
+---@field children BALATRO.Moveable[] | table<string, BALATRO.Moveable>
+---@field STATIONARY? boolean
+---@field config BALATRO.Moveable.Config
+---@field temp_offs BALATRO.Node.Point
+---@field major? BALATRO.Moveable
+---@field zoom? boolean
+local Moveable = {}
 
 
 ---@class BALATRO.Moveable.Class : BALATRO.Node.Super
+---@field init fun(self: BALATRO.Moveable, X: BALATRO.Node.Arguments | number, Y: number, W: number, H: number) Initializes the moveable.
+---@field draw fun(self: BALATRO.Moveable) Draws the moveable.
 ---@field align_to_major fun(self: BALATRO.Moveable) Aligns the moveable to the major axis.
 ---@field hard_set_T fun(self: BALATRO.Moveable, X: number, Y: number, W: number, H: number) Sets the T property of the moveable.
----@field hard_set_VT fun(self: BALATRO.Moveable, X: number, Y: number, W: number, H: number) Sets the VT property of the moveable.
----@field drag fun(self: BALATRO.Moveable, X: number, Y: number) Drags the moveable.
----@field juice_up fun(self: BALATRO.Moveable, amount: number, rot_amt) Juices up the moveable.
+---@field hard_set_VT fun(self: BALATRO.Moveable) Sets the VT property of the moveable.
+---@field drag fun(self: BALATRO.Moveable, offset?: BALATRO.Node.Point) Drags the moveable.
+---@field juice_up fun(self: BALATRO.Moveable, amount: number, rot_amt: number) Juices up the moveable.
 ---@field move_juice fun(self: BALATRO.Moveable, dt: number) Moves the juice of the moveable.
 ---@field move fun(self: BALATRO.Moveable, dt: number) Moves the moveable.
-
---- [Moveable](lua://BALATRO.Moveable) instance.
----@class BALATRO.Moveable.Instance : BALATRO.Moveable
----@field __index BALATRO.Moveable
+---@field lr_clamp fun(self: BALATRO.Moveable)
+---@field glue_to_major fun(self: BALATRO.Moveable, major_tab: BALATRO.Moveable)
+---@field move_with_major fun(self: BALATRO.Moveable, dt: number)
+---@field move_xy fun(self: BALATRO.Moveable, dt: number) Moves the moveable in the x and y direction.
+---@field move_scale fun(self: BALATRO.Moveable, dt: number) Moves the moveable in the scale direction.
+---@field move_wh fun(self: BALATRO.Moveable, dt: number) Moves the moveable in the width and height direction.
+---@field move_r fun(self: BALATRO.Moveable, dt: number, vel: BALATRO.Moveable.Velocity) Moves the moveable in the rotation direction.
+---@field calculate_parrallax fun(self: BALATRO.Moveable) Calculates the shadow parrallax of the moveable.
+---@field get_major fun(self: BALATRO.Moveable): BALATRO.Moveable | BALATRO.Moveable.ARGS.get_major
+---@field remove fun(self: BALATRO.Moveable) Removes the moveable.
 
 
 --- `Moveable()` - creates a new instance of [Moveable](lua://BALATRO.Moveable).
----@param X BALATRO.Node.Arguments | number
----@param Y number
----@param W number
----@param H number
----@return BALATRO.Moveable.Instance
-Moveable = function(X, Y, W, H) end
+---@alias BALATRO.Moveable.Callable BALATRO.Moveable | (fun(X: BALATRO.Node.Arguments | number, Y: number, W: number, H: number): BALATRO.Moveable)
 
 
 --- Extends the class with the child class.
 ---@param self BALATRO.Node
----@return BALATRO.Moveable
+---@return BALATRO.Moveable.Callable
 function Node.extend(self) end
-
-local Node = Object:extend()
-
-
----@param self BALATRO.Node
----@param args BALATRO.Node.Arguments
-function Node.init(self, args) end
 
 
 ---@alias Moveable BALATRO.Moveable
 
 
----@class BALATRO.Moveable.set_alignment.Args : BALATRO.Moveable.Role, BALATRO.Moveable.Alignment
----@field wh_bond? BALATRO.Moveable.Role.Bond
----@field r_bond? BALATRO.Moveable.Role.Bond
----@field scale_bond? BALATRO.Moveable.Role.Bond
+---@class BALATRO.Moveable.MWM
+---@field rotated_offset BALATRO.Node.Point | { x?: number, y?: number }
+---@field angles table
+---@field WH table
+---@field offs BALATRO.Node.Point | { x?: number, y?: number }
+
+
+---@class BALATRO.Moveable.set_alignment.Args : BALATRO.Moveable.Role.Common
+---@field bond? BALATRO.Moveable.Role.Bond
 ---@field lr_clamp? number
+---@field type? BALATRO.Moveable.Alignment.Type
 
 
 --- Sets the alignment of the moveable.
 ---@param self BALATRO.Moveable
----@param args { }
+---@param args BALATRO.Moveable.set_alignment.Args
 function Moveable.set_alignment(self, args) end
+
+
+---@class BALATRO.Moveable.set_role.Args : BALATRO.Moveable.Role.Common
+---@field role_type? BALATRO.Moveable.Role.Type
+---@field draw_major? BALATRO.Moveable
+
+
+--- Sets the role of the moveable.
+---@param self BALATRO.Moveable
+---@param args BALATRO.Moveable.set_role.Args
+function Moveable.set_role(self, args) end

5891fa2faff014e3f618e9d49ade93467ef24577
LnxFCA <dev@lnxfca.com>
2025-03-23 20:20:09 -0500

    balatro-dev: Add some globals

--- balatro-dev/balatro.lua
+++ balatro-dev/balatro.lua
@@ -1,1 +1,14 @@
 ---@meta
+
+
+--- Permanetly discover card
+---@param card? BALATRO.Card.Center | BALATRO.Card
+local function discover_card(card) end
+
+--- Permanetly unlock card
+---@param card? BALATRO.Card.Center | BALATRO.Card
+local function unlock_card(card) end
+
+
+_G.discover_card = discover_card
+_G.unlock_card  = unlock_card

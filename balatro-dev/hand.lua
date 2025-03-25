7f8219bfa0a6f7708eb5fb06c1e3d30111ea7532
LnxFCA <dev@lnxfca.com>
2025-03-24 19:24:49 -0500

    balatro-dev: Initial Hand related types

---
+++ balatro-dev/hand.lua
@@ -0,0 +1,7 @@
+---@meta
+
+---@alias BALATRO.Hand.Type.Flush "Flush Five" | "Flush House" | "Straight Flush" | "Flush"
+---@alias BALATRO.Hand.Type.NKind "Four of a Kind" | "Four of a Kind" | "Three of a Kind" | "High Card"
+---@alias BALATRO.Hand.Type.Other "Full House" | "Straight" | "Two Pair" | "Pair"
+
+---@alias BALATRO.Hand.Type string | BALATRO.Hand.Type.Flush | BALATRO.Hand.Type.NKind | BALATRO.Hand.Type.Other

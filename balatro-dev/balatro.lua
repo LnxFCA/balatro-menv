9c3187306e6cf36c57c8800b13c644f3d4f944d6
LnxFCA <dev@lnxfca.com>
2025-03-25 14:53:04 -0500

    balatro-dev: Update globals

--- balatro-dev/balatro.lua
+++ balatro-dev/balatro.lua
@@ -3,21 +3,22 @@
 
 --- Permanetly discover card
 ---@param card? BALATRO.Card.Center | BALATRO.Card
-local function discover_card(card) end
+_G.discover_card = function(card) end
+
 
 --- Permanetly unlock card
 ---@param card? BALATRO.Card.Center | BALATRO.Card
-local function unlock_card(card) end
+_G.unlock_card = function(card) end
 
 
 --- Create a new Card object
 ---@param _type BALATRO.Card.Ability.Set Card set. See [Card.Ability.Set](lua://BALATRO.Card.Ability.Set)
----@param area? unknown [CardArea](lua://BALATRO.CardArea) where this card will be emplaced.
+---@param area? BALATRO.CardArea [CardArea](lua://BALATRO.CardArea) where this card will be emplaced.
 ---@param legendary? boolean Use legendary pool
 ---@param _rarity? number Card rarity pool
 ---@param skip_materialize? boolean Skit card materialize animation
 ---@param soulable? boolean Soulable animation
----@param forced_key? string Generate specific card instead of a random one.
+---@param forced_key? string | number Generate specific card instead of a random one.
 ---@param key_append? string Append value to generated pools
 ---@return BALATRO.Card
 _G.create_card = function(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append) end
@@ -30,12 +31,21 @@ _G.create_card = function(_type, area, legendary, _rarity, skip_materialize, sou
 _G.pseudoseed = function(key, predict_seed) end
 
 
---- Return a pseudo-random element
+--- Return a pseudo-random number
 ---@param seed string | number
 ---@param min? number
 ---@param max? number
 ---@return number
 _G.pseudorandom = function(seed, min, max) end
 
-_G.discover_card = discover_card
-_G.unlock_card  = unlock_card
+
+--- Prepare graphics for draw
+---@overload fun(moveable: BALATRO.UIElement, scale: number, rotate?: number, offset?: BALATRO.Node.Point)
+---@overload fun(moveable: BALATRO.DynaText, scale: number, rotate?: number, offset?: BALATRO.Node.Point)
+---@overload fun(moveable: BALATRO.Particles, scale: number, rotate?: number, offset?: BALATRO.Node.Point)
+---@param moveable BALATRO.Sprite
+---@param scale number
+---@param rotate? number
+---@param offset? BALATRO.Node.Point
+---@param _? boolean
+_G.prep_draw = function(moveable, scale, rotate, offset, _) end

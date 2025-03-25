86cdbbfe9dff31d51ff62b3ef8c226a0d110149e
LnxFCA <dev@lnxfca.com>
2025-03-25 14:57:54 -0500

    balatro-dev: Add PokerHand type

--- balatro-dev/hand.lua
+++ balatro-dev/hand.lua
@@ -5,3 +5,21 @@
 ---@alias BALATRO.Hand.Type.Other "Full House" | "Straight" | "Two Pair" | "Pair"
 
 ---@alias BALATRO.Hand.Type string | BALATRO.Hand.Type.Flush | BALATRO.Hand.Type.NKind | BALATRO.Hand.Type.Other
+
+
+---@alias BALATRO.Hand.Example [string, boolean]
+
+
+---@class BALATRO.PokerHand
+---@field visible boolean
+---@field order number
+---@field mult number
+---@field chips number
+---@field s_mult number
+---@field s_chips number
+---@field level number
+---@field l_mult number
+---@field l_chips number
+---@field played number
+---@field played_this_round number
+---@field example BALATRO.Hand.Example[]

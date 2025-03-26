69ac7939af7c94a8c946002afc89cf5cf364a400
LnxFCA <dev@lnxfca.com>
2025-03-25 19:28:03 -0500

    balatro-dev: Add Back types

---
+++ balatro-dev/back.lua
@@ -0,0 +1,65 @@
+---@meta
+
+
+---@class BALATRO.Back.TriggerArgs
+---@field context string
+---@field chips? number
+---@field mult? number
+
+
+---@class BALATRO.Back.Effect
+---@field center BALATRO.Center.Back
+---@field text_UI string
+---@field config BALATRO.Center.Back.Config
+
+
+---@class BALATRO.Back.Base
+---@field name string
+---@field effect BALATRO.Back.Effect
+---@field pos BALATRO.Node.Point
+
+
+---@class BALATRO.Back.SaveTable : BALATRO.Back.Base
+---@field key string
+
+
+---@class BALATRO.Back : BALATRO.Back.Class, BALATRO.Back.Base
+---@field loc_name string
+local Back = {}
+
+
+---@class BALATRO.Back.Class : BALATRO.Object.Super
+---@field init fun(self: BALATRO.Back, selected_back: BALATRO.Center.Back)
+---@field get_name fun(self: BALATRO.Back): string?
+---@field change_to fun(self: BALATRO.Back, new_back?: BALATRO.Center.Back)
+---@field save fun(self: BALATRO.Back): BALATRO.Back.SaveTable
+---@field apply_to_run fun(self: BALATRO.Back)
+---@field load fun(self: BALATRO.Back, save_table: BALATRO.Back.SaveTable)
+
+
+--- Generate Back UI
+---@param self BALATRO.Back
+---@param other any
+---@param ui_scale? number
+---@param min_dims? number
+---@param challenge? BALATRO.Challenge
+Back.generate_UI = function(self, other, ui_scale, min_dims, challenge) end
+
+
+--- Trigger Back effect
+---@param self BALATRO.Back
+---@param args BALATRO.Back.TriggerArgs
+---@return number, number
+Back.trigger_effect = function(self, args) end
+
+
+--- Trigger Back effect
+---@param self BALATRO.Back
+---@param args? BALATRO.Back.TriggerArgs
+Back.trigger_effect = function(self, args) end
+
+
+--- `Back()` - Creates a new [Back](lua://BALATRO.Back) object
+---@param selected_back BALATRO.Center.Back
+---@return BALATRO.Back
+_G.Back = function(selected_back) end

e23b3d7c733485d56fabd2fb562dd6345aaf531b
LnxFCA <dev@lnxfca.com>
2025-03-27 13:31:52 -0500

    balatro-dev: Add initial Atlas types

---
+++ balatro-dev/atlas.lua
@@ -0,0 +1,15 @@
+---@meta
+
+
+---@alias BALATRO.Atlas.Animation.Type string
+---@alias BALATRO.Atlas.Assets.Type string
+
+
+---@class BALATRO.Atlas.Base
+
+
+---@class BALATRO.Atlas.Animation : BALATRO.Atlas.Base
+
+
+---@class BALATRO.Atlas.Asset : BALATRO.Atlas.Base
+

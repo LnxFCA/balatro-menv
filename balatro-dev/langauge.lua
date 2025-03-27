d4ec2cbd7c04fd0669ac0e2ed4abc1ca8fcef895
LnxFCA <dev@lnxfca.com>
2025-03-27 13:31:17 -0500

    balatro-dev: Add initial language related types

---
+++ balatro-dev/langauge.lua
@@ -0,0 +1,19 @@
+---@meta
+
+
+---@alias BALATRO.Language.Type string
+---| "de"
+---| "en-us"
+---| "es_419"
+---| "es_ES"
+---| "fr"
+---| "id"
+---| "it"
+---| "ja"
+---| "ko"
+---| "nl"
+---| "pl"
+---| "pt_BR"
+---| "ru"
+---| "zh_CN"
+---| "zh_TW"

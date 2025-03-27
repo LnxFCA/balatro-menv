1d1158e601ed0f6d60ed96f6a35ce16d2e11173f
LnxFCA <dev@lnxfca.com>
2025-03-27 13:29:19 -0500

    balatro-dev: Add extra Hand types

--- balatro-dev/hand.lua
+++ balatro-dev/hand.lua
@@ -3,8 +3,8 @@
 ---@alias BALATRO.Hand.Type.Flush "Flush Five" | "Flush House" | "Straight Flush" | "Flush"
 ---@alias BALATRO.Hand.Type.NKind "Four of a Kind" | "Four of a Kind" | "Three of a Kind" | "High Card"
 ---@alias BALATRO.Hand.Type.Other "Full House" | "Straight" | "Two Pair" | "Pair"
-
 ---@alias BALATRO.Hand.Type string | BALATRO.Hand.Type.Flush | BALATRO.Hand.Type.NKind | BALATRO.Hand.Type.Other
+---@alias BALATRO.HandList table<number, BALATRO.Hand.Type>
 
 
 ---@alias BALATRO.Hand.Example [string, boolean]

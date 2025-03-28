3d435699568cac00c6953c08da65dac4a4ee684e
LnxFCA <dev@lnxfca.com>
2025-03-27 21:29:29 -0500

    balatro-dev: Finish Atlas types

--- balatro-dev/atlas.lua
+++ balatro-dev/atlas.lua
@@ -1,15 +1,42 @@
 ---@meta
 
 
----@alias BALATRO.Atlas.Animation.Type string
----@alias BALATRO.Atlas.Assets.Type string
+---@alias BALATRO.Atlas.Animation.Key string | "blind_chips" | "shop_sign"
+---@alias BALATRO.Atlas.Image.Key string | "playstack_logo" | "localthunk_logo"
+---@alias BALATRO.Atlas.Asset.Key string
+---|"cards_1"
+---|"cards_2"
+---|"centers"
+---|"Joker"
+---|"Tarot"
+---|"Voucher"
+---|"Booster"
+---|"ui_1"
+---|"ui_2"
+---|"balatro"
+---|"gamepad_ui"
+---|"icons"
+---|"tags"
+---|"stickers"
+---|"chips"
+---| BALATRO.Atlas.Image.Key
+
+---@alias BALATRO.Atlas BALATRO.Atlas.Asset | BALATRO.Atlas.Animation
+---@alias BALATRO.Game.Atlas.Animation table<BALATRO.Atlas.Animation.Key, BALATRO.Atlas.Animation>
+---@alias BALATRO.Game.Atlas.Asset table<BALATRO.Atlas.Asset.Key, BALATRO.Atlas.Asset>
 
 
 ---@class BALATRO.Atlas.Base
+---@field image love.Image
+---@field px number
+---@field py number
 
 
 ---@class BALATRO.Atlas.Animation : BALATRO.Atlas.Base
+---@field name BALATRO.Atlas.Animation.Key
+---@field frames number
 
 
 ---@class BALATRO.Atlas.Asset : BALATRO.Atlas.Base
-
+---@field name BALATRO.Atlas.Asset.Key
+---@field type string

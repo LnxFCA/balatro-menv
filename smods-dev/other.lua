c8cc278e6456aed72060fb8211454725a534e147
LnxFCA <dev@lnxfca.com>
2025-04-09 12:03:08 -0500

    smods-dev: Add SMODS.LockVars type

--- smods-dev/other.lua
+++ smods-dev/other.lua
@@ -1,6 +1,9 @@
 ---@meta
 
 
+---@alias SMODS.LocVars SMODS.LocVars.Base | SMODS.LocVars.Extra
+
+
 ---@class SMODS.Settings
 ---@field real_language? BALATRO.Language.Key
 
@@ -8,3 +11,21 @@
 ---@class SMODS.Colours
 ---@field STAKES BALATRO.UI.Colour[]
 ---@field RARITY table<string, BALATRO.UI.Colour>
+
+
+---@class SMODS.LocVars.Other
+---@field colours? BALATRO.UI.Colour[]
+
+
+---@class SMODS.LocVars.Base
+---@field vars string[] | SMODS.LocVars.Other
+---@field key? string
+---@field set string
+---@field scale? number
+---@field text_colour? BALATRO.UI.Colour
+---@field background_colour? BALATRO.UI.Colour
+---@field main_start? BALATRO.UI.Node[]
+---@field main_end? BALATRO.UI.Node[]
+
+
+---@class SMODS.LocVars.Extra

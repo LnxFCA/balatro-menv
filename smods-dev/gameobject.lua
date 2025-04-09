dd382e7d83294884e10fc4f1852e08fbeb02517d
LnxFCA <dev@lnxfca.com>
2025-04-09 09:29:02 -0500

    smods-dev: Update SMODS.GameObject

--- smods-dev/gameobject.lua
+++ smods-dev/gameobject.lua
@@ -1,10 +1,14 @@
 ---@meta
 
 
----@alias SMODS.GameObject.Arguments SMODS.GameObject.Arguments.Base | SMODS.GameObject.Arguments.Extra
 ---@alias SMODS.RequiredParams.Type string[]
 ---@alias SMODS.GObject SMODS.GameObject | SMODS.GameObject.Subclasses
+---@alias SMODS.GameObject.Config SMODS.GameObject.Config.Base | SMODS.GameObject.Config.Extra
+---@alias SMODS.GameObject.PrefixConfig SMODS.GameObject.PrefixConfig.Base | SMODS.GameObject.PrefixConfig.Extra
 ---@alias SMODS.GameObject.Subclasses
+---| SMODS.GameObject.Super
+---| SMODS.ObjectType
+---| SMODS.ConsumeableType
 ---| SMODS.Achievement
 ---| SMODS.Atlas
 ---| SMODS.Blind
@@ -37,12 +41,8 @@
 ---| SMODS.Tag
 
 ---@alias SMODS.GameObject.Set string
-
-
----@class SMODS.GameObject.Arguments.Base
-
-
----@class SMODS.GameObject.Arguments.Extra
+---@alias SMODS.GameObject.OwnershipType SMODS.GameObject.Base | SMODS.GameObject.Empty
+---@alias SMODS.GameObject.PrefixConfig.Effect SMODS.GameObject.PrefixConfig.Effect.Base | boolean
 
 
 ---@class SMODS.GameObject.Super : SMODS.GameObject
@@ -50,36 +50,70 @@
 ---@field super SMODS.GameObject
 
 
----@class SMODS.GameObject : SMODS.GameObject.Class
----@field __index SMODS.GameObject
+---@class SMODS.GameObject.Config.Base
+
+
+---@class SMODS.GameObject.Config.Extra
+
+
+---@class SMODS.GameObject.PrefixConfig.Effect.Base
+---@field mod? boolean
+---@field class? boolean
+
+
+---@class SMODS.GameObject.PrefixConfig.Base
+---@field key? SMODS.GameObject.PrefixConfig.Effect
+---@field atlas? SMODS.GameObject.PrefixConfig.Effect
+---@field shader? SMODS.GameObject.PrefixConfig.Effect
+---@field card_key? SMODS.GameObject.PrefixConfig.Effect
+---@field above_stake? SMODS.GameObject.PrefixConfig.Effect
+---@field applied_stakes? SMODS.GameObject.PrefixConfig.Effect | table<number, SMODS.GameObject.PrefixConfig.Effect>
+
+
+---@class SMODS.GameObject.PrefixConfig.Extra
+---@field unlocked_stake? SMODS.GameObject.PrefixConfig.Effect
+
+
+--- Common properties for `GameObject` subclasses
+---@class SMODS.GameObject.Extra : SMODS.GameObject.Common
+---@field required_params? SMODS.RequiredParams.Type Not in `SMODS.GameObject`
+---@field set SMODS.GameObject.Set Not in `SMODS.GameObject`
+---@field key string Not in `SMODS.GameObject`
+---@field registered boolean Not in `SMODS.GameObject`
+---@field obj_table table<string, SMODS.GObject> Not in `SMODS.GameObject`
+---@field obj_buffer string[] Not in `SMODS.GameObject`
+---@field raw_key? string Not in `SMODS.GameObject`
+---@field mod SMODS.Mod Not in `SMODS.GameObject`
+---@field original_key string Not in `SMODS.GameObject`
+---@field class_prefix string Not in `SMODS.GameObject`
+---@field log_interval? number Not in `SMODS.GameObject`
+---@field taken_ownership? boolean Not it `SMODS.GameObject`
+---@field rarity_original? number Not in `SMODS.GameObject`
+
+
+---@class SMODS.GameObject.Common
+---@field name? string
+---@field loc_txt? table Not in `SMODS.GameObject`
+---@field unlocked? boolean Not in `SMODS.GameObject`
+---@field discovered? boolean Not in `SMODS.GameObject`
+---@field no_collection? boolean Not in `SMODS.GameObject`
+---@field config? SMODS.GameObject.Config Not in `SMODS.GameObject`
+---@field prefix_config? SMODS.GameObject.PrefixConfig | boolean Not in `SMODS.GameObject`
+---@field dependencies? string | string[] Not in `SMODS.GameObject`
+---@field display_size? BALATRO.UI.Resolution
+---@field pixel_size? BALATRO.UI.Resolution
+
+
+---@class SMODS.GameObject : SMODS.GameObject.Class, SMODS.GameObject.Extra
 ---@field subclasses SMODS.GameObject.Subclasses[]
----@field mod SMODS.Mod
----@field original_mod SMODS.Mod
----@field required_params? SMODS.RequiredParams.Type
----@field set SMODS.GameObject.Set
----@field key string
----@field obj_table table<string, SMODS.GObject>
----@field obj_buffer string[]
----@field registered boolean
----@field dependencies string | string[]
----@field raw_key? string
----@field class_prefix string
----@field original_key string
----@field log_interval number
----@field is_loc_modified boolean
----@field taken_ownership boolean
----@field _d? boolean
----@field _u? boolean
----@field _saved_d_u boolean
----@field discovered boolean
----@field unlocked boolean
----@field _discovered_unlocked_overwritten? boolean
----@field atlas? string
+---@field required_params? string[]
 local GameObject = {}
 
 
----@class SMODS.GameObject.Class : BALATRO.Object.Super
----@field extend fun(self: SMODS.GameObject, args?: table<string, any>): SMODS.GameObject.Super
+---@class SMODS.GameObject.Empty
+
+
+---@class SMODS.GameObject.Base
 ---@field check_duplicate_register fun(self: SMODS.GameObject): boolean
 ---@field check_duplicate_key fun(self: SMODS.GameObject): boolean
 ---@field get_obj? fun(self: SMODS.GameObject, key: string): SMODS.GameObject
@@ -88,7 +122,12 @@ local GameObject = {}
 ---@field process_loc_text fun(self: SMODS.GameObject)
 ---@field send_to_subclasses fun(self: SMODS.GameObject, func?: string, ...)
 ---@field inject_class fun(self: SMODS.GameObject)
----@field take_ownership fun(self: SMODS.GameObject, key: string, obj: SMODS.GObject, silent?: boolean): SMODS.GObject?
+
+
+---@class SMODS.GameObject.Class : BALATRO.Object.Super, SMODS.GameObject.Base
+---@field __index SMODS.GameObject
+---@field take_ownership fun(self: SMODS.GameObject, key: string, obj: SMODS.GameObject.OwnershipType, silent?: boolean): SMODS.GameObject?
+---@field extend fun(self: SMODS.GameObject, args?: table<string, any>): SMODS.GameObject.Super
 
 
 --- Return a list of instances of the object class
@@ -100,15 +139,16 @@ GameObject.obj_list = function(self, reversed) end
 
 
 --- `SMODS.GameObject` - Metatable `__call`
----@overload fun(self: SMODS.GameObject, obj: SMODS.GameObject): SMODS.GameObject?
+---@overload fun(self: SMODS.GameObject, obj: table<string, any>): SMODS.GameObject?
 ---@param self SMODS.GameObject
----@param obj? SMODS.GameObject
+---@param obj? table<string, any>
 ---@return SMODS.GameObject
 GameObject.__call = function(self, obj) end
 
 
 --- `SMODS.GameObject()` - Creates a  new [SMODS.GameObject](lua://SMODS.GameObject) object
 ---@param self SMODS.GameObject
----@param args? SMODS.GameObject.Arguments
+---@param args? table<string, unknown>
 ---@return SMODS.GameObject
 _G.SMODS.GameObject = function(self, args) end
+_G.SMODS.GameObject = GameObject

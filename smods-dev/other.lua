dd1c6247407fe864bdbf9fed739f61a4953de5fa
LnxFCA <dev@lnxfca.com>
2025-04-09 09:34:41 -0500

    smods-dev: Add misc types

---
+++ smods-dev/other.lua
@@ -0,0 +1,10 @@
+---@meta
+
+
+---@class SMODS.Settings
+---@field real_language? BALATRO.Language.Key
+
+
+---@class SMODS.Colours
+---@field STAKES BALATRO.UI.Colour[]
+---@field RARITY table<string, BALATRO.UI.Colour>
